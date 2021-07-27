8ef7eb7b7cf265b41463831e09bff45a52b4f31c
Simonbelete <simonbelete@gmail.com>
2021-07-27 04:44:31 +0300

    badassshop - fixing export in molecules

---
+++ badassshop/src/components/molecules/PrimaryNavigation/component.tsx
@@ -0,0 +1,25 @@
+import React, { ReactElement } from 'react';
+
+import { Button } from '../../atoms';
+import { Dropdown } from '../../molecules';
+import style from './style.module.sass';
+
+const PrimaryNavigation = () => {
+  const menus = [
+    <a href="#">Product 1</a>,
+    <a href="#">Product 1</a>,
+    <a href="#">Product 1</a>,
+  ];
+  return (
+    <div className={style.primarynavigation}>
+      <div className={style.primarynavigation_left}>
+        <Dropdown menus={menus}>Products</Dropdown>
+        <Button>Contact</Button>
+      </div>
+      <div className={style.primarynavigation_center}></div>
+      <div className={style.primarynavigation_right}></div>
+    </div>
+  );
+};
+
+export default PrimaryNavigation;
